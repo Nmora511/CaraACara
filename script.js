64e8082d06ed40c2729754dba37cbf3bdf141b79
Neto Moraes <102070201+Nmora511@users.noreply.github.com>
2023-09-07 05:37:05 -0300

    Add files via upload
    Version (0.0.5) - Alpha
    - Drawing Button
    - Help Button
    - Images can now be uploaded ny pressing the right-button;

--- script.js
+++ script.js
@@ -17,19 +17,62 @@ function updateImage(imageId, input) {
     }
 }
 
-function uploadImages() {
-    for (var i = 1; i <= 18; i++) {
-        var inputId = 'fileInput' + i;
-        var imageId = 'user' + i;
-        var input = document.getElementById(inputId);
-
-        if (input.files && input.files[0]) {
-            updateImage(imageId, input);
-        }
-    }
-}
-
 function triggerFileInput(inputId) {
     var input = document.getElementById(inputId);
     input.click();
+} 
+
+function showContextMenu(event) {
+    event.preventDefault();
+    var contextMenu = document.getElementById('contextMenu');
+    contextMenu.style.display = 'block';
+    contextMenu.style.left = event.pageX + 'px';
+    contextMenu.style.top = event.pageY + 'px';
+
+    window.addEventListener('click', function hideContextMenu() {
+        contextMenu.style.display = 'none';
+        window.removeEventListener('click', hideContextMenu);
+    });
+}
+
+function sortearImagem() {
+    const idsImagens = [
+        'user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8',
+        'user9', 'user10', 'user11', 'user12', 'user13', 'user14', 'user15', 'user16', 'user17', 'user18'
+    ];
+
+    const indiceSorteado = Math.floor(Math.random() * idsImagens.length);
+    const idImagemSorteada = idsImagens[indiceSorteado];
+
+    idsImagens.forEach(id => {
+        const imagem = document.getElementById(id);
+        if(imagem.classList.contains('chosen')){
+            borderChosen(id);
+        }
+        if (id != idImagemSorteada) {
+            toggleOpacity(id);
+        }
+        else{
+            borderChosen(id);
+        }
+    });
+
+    setTimeout(function () {
+        idsImagens.forEach(id => {
+            const imagem = document.getElementById(id);
+            if(imagem.classList.contains('opacity')){
+            toggleOpacity(id);
+            }    
+        });
+    }, 5000);
+}
+
+function borderChosen(id){
+    var imagem = document.getElementById(id);
+    imagem.classList.toggle('chosen');
+}
+
+function Help(id){
+    var help = document.getElementById(id);
+    help.classList.toggle('disappear');
 }
